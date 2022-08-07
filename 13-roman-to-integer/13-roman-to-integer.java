1c5f9cd2439665759df2d6f7b12f920e7dc899b2
Seo Do Gyeong <dgseo8981@gmail.com>
2022-08-07 22:15:43 +0900

    Time: 24 ms (9.62%), Space: 52.1 MB (5.00%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,80 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<String, Integer> refer = new HashMap<String, Integer>();
+        refer.put("I", 1);
+        refer.put("V", 5);
+        refer.put("X", 10);
+        refer.put("L", 50);
+        refer.put("C", 100);
+        refer.put("D", 500);
+        refer.put("M", 1000);
+        refer.put("IV", 4);
+        refer.put("IX", 9);
+        refer.put("XL", 40);
+        refer.put("XC", 90);
+        refer.put("CD", 400);
+        refer.put("CM", 900);
+        
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+//             if(s.charAt(i) == 'I'){
+//                 if(s.charAt(i + 1) == 'V'){
+                    
+//                 }
+//             }
+            
+            String letter = s.charAt(i) + "";
+            String temp = "";
+            
+            if(i < s.length() - 1){
+                temp = s.charAt(i + 1) + "";
+            }
+            
+            if(letter.equals("C")){
+                if(temp.equals("M")){
+                    result += refer.get(letter + temp);
+                    i++;
+                    continue;
+                }
+                
+                if(temp.equals("D")){
+                    result += refer.get(letter + temp);
+                    i++;
+                    continue;
+                }
+            }
+            
+            if(letter.equals("X")){
+                if(temp.equals("C")){
+                    result += refer.get(letter + temp);
+                    i++;
+                    continue;
+                }
+                
+                if(temp.equals("L")){
+                    result += refer.get(letter + temp);
+                    i++;
+                    continue;
+                }
+            }
+            
+            if(letter.equals("I")){
+                if(temp.equals("V")){
+                    result += refer.get(letter + temp);
+                    i++;
+                    continue;
+                }
+                
+                if(temp.equals("X")){
+                    result += refer.get(letter + temp);
+                    i++;
+                    continue;
+                }
+            }
+            
+            result += refer.get(s.charAt(i) + "");
+        }
+        
+        return result;
+    }
+}
