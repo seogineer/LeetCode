610136b0a30c13fadd712d83237b7f4e008bb848
Seo Do Gyeong <dgseo8981@gmail.com>
2022-08-10 23:56:42 +0900

    Time: 1447 ms (5.00%), Space: 83.3 MB (10.49%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0){ return 0; }
+        if(s.length() == 1){ return 1; }
+        
+        int answer = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(answer > s.length() - (i + 1)){
+                break;
+            }
+            
+            List<Character> list = new LinkedList<>();
+            for(int j = i; j < s.length(); j++){
+                if(!list.contains(s.charAt(j))){
+                    list.add(s.charAt(j));
+
+                    if(j == s.length() - 1){
+                        answer = Math.max(answer, list.size());
+                    }
+                } else {
+                    if(answer < list.size()){
+                        answer = list.size();
+                    }
+                    
+                    break;
+                    // list.clear();
+                    // list.add(s.charAt(j));
+                }    
+            }
+        }
+        
+        return answer;
+    }
+}
