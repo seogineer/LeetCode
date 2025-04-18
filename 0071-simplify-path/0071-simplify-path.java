c0f36aecd459813e8d87f200875c7f59c5d5518e
DoGyeong Seo <dgseo8981@gmail.com>
2025-04-18 19:14:47 +0900

    Time: 4 ms (85.66%), Space: 43.4 MB (73.1%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] parts = path.split("/");
+        // System.out.println(Arrays.toString(parts));
+        for (String part : parts) {
+            if ("".equals(part) || ".".equals(part)) {
+                continue;
+            }
+            if ("..".equals(part)) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+                continue;
+            }
+            stack.push(part);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String part : stack) {
+            sb.append("/").append(part);
+        }
+        return "".equals(sb.toString()) ? "/" : sb.toString();
+    }
+}
