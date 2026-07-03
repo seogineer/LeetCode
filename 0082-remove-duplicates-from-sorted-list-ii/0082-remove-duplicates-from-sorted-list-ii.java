6592e7cea7c507a4785f88cfeff4cab60bb385c1
DoGyeong Seo <dgseo8981@gmail.com>
2026-07-03 21:14:19 +0900

    Time: 0 ms (100%), Space: 45 MB (37.54%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,26 +10,22 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        Map<Integer, Integer> map = new HashMap<>();
-
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
         ListNode curr = head;
-        while (curr != null) {
-            map.put(curr.val, (map.get(curr.val) == null ? 0 : map.get(curr.val)) + 1);
-            curr = curr.next;
-        };
 
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-
-        curr = head;
         while (curr != null) {
-            if (map.get(curr.val) == 1) {
-                tail.next = curr;
-                tail = tail.next;
+            if (curr.next != null && curr.val == curr.next.val) {
+                int dupVal = curr.val;
+                while (curr != null && curr.val == dupVal) {
+                    curr = curr.next;
+                }
+                prev.next = curr;
+            } else {
+                prev = curr;
+                curr = curr.next;
             }
-            curr = curr.next;
         }
-        tail.next = null;
 
         return dummy.next;
     }
